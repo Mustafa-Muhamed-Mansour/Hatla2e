ae8300a7628250130f89e71e6b3112e07902b730
Mustafa-Muhamed-Mansour <mostafamansour163@yahoo.com>
2023-01-16 19:28:48 +0200

    remove constant apiKey

--- app/src/main/java/com/lafa/common/Constant.kt
+++ app/src/main/java/com/lafa/common/Constant.kt
@@ -4,7 +4,6 @@ class Constant {
 
 
     companion object {
-        const val BASE_URL = "https://student.valuxapps.com/api/"
         const val PRODUCT = "PRODUCT"
         const val PRODUCT_TABLE = "PRODUCT_TABLE"
         const val USER_PREF = "UserPref"
